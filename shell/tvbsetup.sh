0f531635727e7c87f072d62df9a2036d9f3f6283
umar <umarbrowser20@gmail.com>
2017-12-28 09:19:27 +0100

    Added comments on tvbsetup.sh

--- shell/tvbsetup.sh
+++ shell/tvbsetup.sh
@@ -2,15 +2,19 @@
 
 echo "Installing TVB Packages on virtual machine..."
 
+# curl
 echo "Installing Curl"
 apt-get install curl -y
 
+# miniconda
 echo "Downloading Miniconda"
 cd /tmp
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 
+# tvb
 echo "Installing TVB packages using conda"
 conda install -c conda-forge tvb-framework
 
+# end
 echo "Finished Installing TVB Packages."
